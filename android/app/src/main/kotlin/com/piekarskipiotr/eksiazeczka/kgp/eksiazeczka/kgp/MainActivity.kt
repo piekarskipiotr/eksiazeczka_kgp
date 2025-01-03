395b3f5c6c6a2874c9398e54c251a83f9bec8c79
piekarskipiotr <ppiekarski8@gmail.com>
2025-01-03 12:37:13 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/piekarskipiotr/eksiazeczka/kgp/eksiazeczka/kgp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.piekarskipiotr.eksiazeczka.kgp.eksiazeczka_kgp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:eksiazeczka_kgp/app/app.dart';
+import 'package:eksiazeczka_kgp/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
