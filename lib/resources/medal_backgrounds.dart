5828fdfaf702fb26e0b804f0ab97239780ad0d93
piekarskipiotr <ppiekarski8@gmail.com>
2025-01-20 15:35:08 +0100

    Add new images

--- lib/resources/medal_backgrounds.dart
+++ lib/resources/medal_backgrounds.dart
@@ -3,9 +3,9 @@ part of 'resources.dart';
 class MedalBackgrounds {
   MedalBackgrounds._();
 
-  static const String all = 'assets/images/medalBackgrounds/all.jpeg';
-  static const String fifth = 'assets/images/medalBackgrounds/fifth.jpeg';
-  static const String first = 'assets/images/medalBackgrounds/first.jpeg';
-  static const String tenth = 'assets/images/medalBackgrounds/tenth.jpeg';
-  static const String twenty = 'assets/images/medalBackgrounds/twenty.jpeg';
+  static const String all = 'assets/images/medalBackgrounds/all.jpg';
+  static const String fifth = 'assets/images/medalBackgrounds/fifth.jpg';
+  static const String first = 'assets/images/medalBackgrounds/first.jpg';
+  static const String tenth = 'assets/images/medalBackgrounds/tenth.jpg';
+  static const String twenty = 'assets/images/medalBackgrounds/twenty.jpg';
 }
