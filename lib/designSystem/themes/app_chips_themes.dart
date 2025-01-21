a9655888cc9c483aa66bf3fdf6a7f9830fd3b372
piekarskipiotr <ppiekarski8@gmail.com>
2025-01-21 10:27:12 +0100

    Fix chips label lag

--- lib/designSystem/themes/app_chips_themes.dart
+++ lib/designSystem/themes/app_chips_themes.dart
@@ -1,14 +1,13 @@
-import 'package:eksiazeczka_kgp/designSystem/design_system.dart';
 import 'package:flutter/material.dart';
 
 class AppChipsThemes {
   static ChipThemeData chipThemeData({required Color primary, required Color scaffoldBackground}) {
     return ChipThemeData(
+      backgroundColor: scaffoldBackground,
       color: WidgetStateProperty.resolveWith<Color>((states) {
         if (states.contains(WidgetState.selected)) return primary;
         return scaffoldBackground;
       }),
-      labelStyle: AppTextStyles.h8(color: primary),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(48),
         side: BorderSide(color: primary),
